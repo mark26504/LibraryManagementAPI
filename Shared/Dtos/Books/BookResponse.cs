f7dca4140bbed66d9333044b7df1904a2f3cf10b
Mark Sameeh <141139719+mark26504@users.noreply.github.com>
2026-09-02 20:19:33 +0300

    Update BookResponse

--- Shared/Dtos/Books/BookResponse.cs
+++ Shared/Dtos/Books/BookResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace LibraryManagement.Shared.Dtos.Books
 {
     public record BookResponse
-        (Guid Id, string Title, string ISBN, string Description,
+        (Guid Id, string Title, string ISBN, string Description, string? CoverImageUrl,
          Guid CategoryId, bool IsActive, DateTime CreatedAt, DateTime? UpdatedAt);
 }
